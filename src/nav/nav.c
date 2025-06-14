1c67e264d5f1f02f9d040bc0e87316fdbf1499f0
gorga.nathan05@gmail.com <gorga.nathan05@gmail.com>
2025-06-14 12:45:04 +0200

    get and list folder contents

--- src/nav/nav.c
+++ src/nav/nav.c
@@ -1,9 +1,45 @@
 #include "nav.h"
 
+
+
+folderContent * createFolderContent(){
+    folderContent * content = (folderContent*)malloc(sizeof(folderContent));
+    content->next = NULL;
+    return content;
+}
+
 void listFolderContents(const char const* folderpath){
+    folderContent * head = createFolderContent();
+
+    getFolderContents(folderpath, head);
+
+    folderContent * curr = head;
+    folderContent * prev = NULL;   
+    while(curr != NULL){
+
+        if (curr->content.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) printf(YELLOW);
+        printf("%s  ", curr->content.cFileName);
+        prev = curr;
+        curr = curr->next;
+        free(prev);
+
+        printf(RESET);
+    }
+
+    
+}
+
+
+void getFolderContents(const char const* folderpath, folderContent * head){
+    
+
+    folderContent * curr = head;
+    curr->next = NULL;
+
     WIN32_FIND_DATA findFileData;
     HANDLE hFind;
 
+    
     // Build search pattern like: "C:\\path\\to\\folder\\*"
     char searchPath[MAX_PATH];
     snprintf(searchPath, MAX_PATH, "%s\\*", folderpath);
@@ -12,25 +48,26 @@ void listFolderContents(const char const* folderpath){
 
     if (hFind == INVALID_HANDLE_VALUE) {
         printf("No files found in %s\n", folderpath);
+        head = NULL;
         return;
     }
 
+    
     do {
         const char *name = findFileData.cFileName;
 
         // Skip "." and ".."
         if (strcmp(name, ".") != 0 && strcmp(name, "..") != 0) {
-            if (findFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
-                printf("[DIR]  %s\n", name);
-            } else {
-                printf("       %s\n", name);
-            }
+            
+            curr->content = findFileData;
+            curr->next = createFolderContent();
+            curr = curr->next; 
+            
         }
     } while (FindNextFile(hFind, &findFileData));
 
     FindClose(hFind);
 }
-
 
 
 void start_nav(const char const* codebase_location){
--- src/nav/nav.h
+++ src/nav/nav.h
@@ -3,6 +3,14 @@
 
 #include "../definitions.h"
 
+
+typedef struct folderContent{
+    WIN32_FIND_DATA content;
+    struct folderContent *next;
+}folderContent;
+
 void listFolderContents(const char const* folderpath);
+
+void getFolderContents(const char const* folderpath, folderContent * head);
 
 #endif
